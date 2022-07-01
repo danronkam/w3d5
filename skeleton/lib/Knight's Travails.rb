e6fd09c2dfba248a9ad68fe5d95831cfa0823cc5
bob-skywalker <q463250938@gmail.com>
2022-07-01 14:48:06 -0700

    test

--- skeleton/lib/Knight's Travails.rb
+++ skeleton/lib/Knight's Travails.rb
@@ -1,10 +1,15 @@
+require_relative "00_tree_node.rb"
+
 class KnightPathFinder
 
     def initialize(pos)
-        @board = 
+        @board = Array.new(8) {Array.new(8)}
         @pos = pos
         @row = pos[0]
         @col = pos[1]
+    end
+    
+    d
 
 
 
