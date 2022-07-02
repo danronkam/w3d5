bf14a5e4146a490cc364d168c44efb3c41e58e68
bob-skywalker <q463250938@gmail.com>
2022-07-01 17:13:11 -0700

    final update

--- skeleton/lib/Knight's_Travails.rb
+++ skeleton/lib/Knight's_Travails.rb
@@ -2,6 +2,7 @@ require_relative "00_tree_node.rb"
 require 'byebug'
 
 class KnightPathFinder
+    attr_reader :considered_pos
 
     POSSIBLEMOVE = [[2, 1],[2,-1], [-2, 1], [-2, -1], [1,2], [-1, 2], [1, -2], [-1,-2]]
 
@@ -16,7 +17,7 @@ class KnightPathFinder
         end
         real_moves = []
         temp.each.with_index do |move,i|
-            real_moves << move if move.all?{|num| num >= 0 && num < 8}
+            real_moves << move if move.all?{|num| num.between?(0, 7)}
         end
 
         real_moves
@@ -39,19 +40,36 @@ class KnightPathFinder
 
     def new_move_positions(pos)
         valid = KnightPathFinder.valid_moves(pos)
-        filtered = []
+        new_moves = []
 
         valid.each do |position|
             if !@considered_pos.include?(position)
-                filtered << position
+                new_moves << position
             end
         end
 
-        @considered_pos += filtered
-        filtered
+        @considered_pos += new_moves
+        new_moves
     end
     
+    def build_move_tree
+        newChildren = self.new_move_positions(@pos)
+ 
+        newChildren.each do |position|
+            position.build_move_tree
 
+
+
+    #     newChildren.each do |pair|
+    #         queue << pair
+    #     end
+
+    #     while !queue.empty?
+    #             popped = queue.shift
+    #             move_tree << self.new_move_positions(popped)
+    #     end
+    #     move_tree
+    # end
 
 
 
